a123a90474888550522f123ae1661428a6f94154
Priyankar Kumar <priyankar.kumar98@gmail.com>
2018-05-09 15:19:57 +0530

    fixed indentation

--- PhonePi.py
+++ PhonePi.py
@@ -1,18 +1,25 @@
-from flask import Flask 
+import sys
+import numpy as np
+import pandas as pd
+import matplotlib
+#matplotlib.use('macosx')
+import matplotlib.pyplot as plt
+
+from flask import Flask
 from flask_sockets import Sockets
 
 
-app = Flask(__name__) 
+app = Flask(__name__)
 sockets = Sockets(app)
 
-@sockets.route('/accelerometer') 
+@sockets.route('/accelerometer')
 def echo_socket(ws):
 	f=open("accelerometer.txt","a")
 	while True:
 		message = ws.receive()
-		print(message) 
-		ws.send(message)
-		print(message, file=f)
+                print(message)
+                ws.send(message)
+                print>>f,message
 	f.close()
 
 
@@ -23,17 +30,17 @@ def echo_socket(ws):
 		message = ws.receive()
 		print(message)
 		ws.send(message)
-		print(message, file=f)
+		print>>f,message
 	f.close()
-	
+
 @sockets.route('/magnetometer')
 def echo_socket(ws):
 	f=open("magnetometer.txt","a")
 	while True:
 		message = ws.receive()
 		print(message)
-		ws.send(message)
-		print(message, file=f)
+                ws.send(message)
+                print>>f,message
 	f.close()
 
 @sockets.route('/orientation')
@@ -41,9 +48,9 @@ def echo_socket(ws):
 	f=open("orientation.txt","a")
 	while True:
 		message = ws.receive()
-		print(message)
-		ws.send(message)
-		print(message, file=f)
+                print(message)
+                ws.send(message)
+                print>>f,message
 	f.close()
 
 @sockets.route('/stepcounter')
@@ -51,9 +58,9 @@ def echo_socket(ws):
 	f=open("stepcounter.txt","a")
 	while True:
 		message = ws.receive()
-		print(message)
-		ws.send(message)
-		print(message, file=f)
+                print(message)
+                ws.send(message)
+                print>>f,message
 	f.close()
 
 @sockets.route('/thermometer')
@@ -61,9 +68,9 @@ def echo_socket(ws):
 	f=open("thermometer.txt","a")
 	while True:
 		message = ws.receive()
-		print(message)
-		ws.send(message)
-		print(message, file=f)
+                print(message)
+                ws.send(message)
+                print>>f,message
 	f.close()
 
 @sockets.route('/lightsensor')
@@ -71,9 +78,9 @@ def echo_socket(ws):
 	f=open("lightsensor.txt","a")
 	while True:
 		message = ws.receive()
-		print(message)
-		ws.send(message)
-		print(message, file=f)
+                print(message)
+                ws.send(message)
+                print>>f,message
 	f.close()
 
 @sockets.route('/proximity')
@@ -81,9 +88,9 @@ def echo_socket(ws):
 	f=open("proximity.txt","a")
 	while True:
 		message = ws.receive()
-		print(message)
-		ws.send(message)
-		print(message, file=f)
+                print(message)
+                ws.send(message)
+                print>>f,message
 	f.close()
 
 @sockets.route('/geolocation')
@@ -91,15 +98,15 @@ def echo_socket(ws):
 	f=open("geolocation.txt","a")
 	while True:
 		message = ws.receive()
-		print(message)
-		ws.send(message)
-		print(message, file=f)
+                print(message)
+                ws.send(message)
+                print>>f,message
 	f.close()
 
-	
 
-@app.route('/') 
-def hello(): 
+
+@app.route('/')
+def hello():
 	return 'Hello World!'
 
 if __name__ == "__main__":
