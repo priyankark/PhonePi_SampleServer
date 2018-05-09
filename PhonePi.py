a3b8727909bfa9d3d6044123502a82e62c318a10
Priyankar Kumar <priyankar.kumar98@gmail.com>
2018-05-09 15:25:46 +0530

    updated Server, fixed indentation

--- PhonePi.py
+++ PhonePi.py
@@ -1,10 +1,6 @@
 import sys
 import numpy as np
 import pandas as pd
-import matplotlib
-#matplotlib.use('macosx')
-import matplotlib.pyplot as plt
-
 from flask import Flask
 from flask_sockets import Sockets
 
