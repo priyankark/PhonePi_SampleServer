2d7d2c2e5602c06c54851020310230b766ef184c
Priyankar Kumar <priyankar.kumar98@gmail.com>
2020-12-26 18:28:36 +0530

    bug fix: change mechanism for getting IP address

--- Python/PhonePi.py
+++ Python/PhonePi.py
@@ -2,8 +2,20 @@ from flask import Flask
 from flask_sockets import Sockets
 import socket
 
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except Exception:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
+
 hostname = socket.gethostname()
-IPAddr = socket.gethostbyname(hostname)
+IPAddr = get_ip()
 print("Your Computer Name is: " + hostname)
 print("Your Computer IP Address is: " + IPAddr)
 print("Enter {}:5000 in the app and select the sensors to stream.".format(IPAddr))
