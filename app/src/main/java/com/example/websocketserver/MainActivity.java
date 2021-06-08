cb45550246f35945f79103d359e95059dcef5825
Ayan <patelayan2030@gamil.com>
2021-06-08 23:28:44 +0530

    server Implemented

---
+++ app/src/main/java/com/example/websocketserver/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.websocketserver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+
+import de.greenrobot.event.EventBus;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MyApplication";
+    private static final int SERVER_PORT = 12345;
+
+    private MySocketServer mServer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        startServer();
+    }
+    private void startServer() {
+        InetAddress inetAddress = getInetAddress();
+        if (inetAddress == null) {
+            Log.e(TAG, "Unable to lookup IP address");
+            return;
+        }
+
+        mServer = new MySocketServer(new InetSocketAddress(inetAddress.getHostAddress(), SERVER_PORT));
+        mServer.start();
+    }
+
+    private static InetAddress getInetAddress() {
+        try {
+            for (Enumeration en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements();) {
+                NetworkInterface networkInterface = (NetworkInterface) en.nextElement();
+
+                for (Enumeration enumIpAddr = networkInterface.getInetAddresses(); enumIpAddr.hasMoreElements();) {
+                    InetAddress inetAddress = (InetAddress) enumIpAddr.nextElement();
+
+                    if (!inetAddress.isLoopbackAddress() && inetAddress instanceof Inet4Address) {
+                        return inetAddress;
+                    }
+                }
+            }
+        } catch (SocketException e) {
+            e.printStackTrace();
+            Log.e(TAG, "Error getting the network interface information");
+        }
+
+        return null;
+    }
+}
